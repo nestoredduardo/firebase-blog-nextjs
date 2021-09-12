36b2175c7b12148243993bf345a868d6d90420e2
Nestor Mamani <nmamanipantoja@gmail.com>
2021-09-11 22:58:50 -0500

    Add blog to Cloud Firestore

--- components/CreatePost.tsx
+++ components/CreatePost.tsx
@@ -1,35 +1,64 @@
-import {useState} from 'react'
+//Documentation : https://firebase.google.com/docs/firestore/quickstart#web-v9_2
+import { useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore/lite";
 
-import fire from '@config/fire-conf';
+import firebaseConfig from "@config/fire-conf";
 
-const CreatePost = ()=>{
-  const [title, setTitle] = useState('')
-  const [content, setContent] = useState('')
+const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [notification, setNotification] = useState("");
 
-  const handleSubmit = (e)=>{
-    e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    fire.firestore()
-      .collection('blog')
-      .add({
-        title: title,
-        content: content,
-      })
+    try {
+      const firebaseApp = initializeApp(firebaseConfig);
+      const db = getFirestore(firebaseApp);
 
-    setTitle('')
-    setContent('')
-  }
+      const docRef = await addDoc(collection(db, "blog"), {
+        title,
+        content,
+      });
 
-  return <div>
-    <h2>Add Blog</h2>
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="title">Title</label>
-      <input type="text" value={title} onChange={({target})=> setTitle(target.value)} />
-      <label htmlFor="content">Content</label>
-      <textarea name="content" value={content} onChange={({target})=> setContent(target.value)} id="1" cols={30} rows={10}></textarea>
-      <button type='submit'>Create Blog</button>
-    </form>
-  </div>
-}
+      setTitle("");
+      setContent("");
 
-export default CreatePost
+      setNotification("Blogpost created with id " + docRef.id);
+
+      setTimeout(() => {
+        setNotification("");
+      }, 2000);
+    } catch (e) {
+      console.log("Error adding document: ", e);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Add Blog</h2>
+      {notification}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
+        />
+        <label htmlFor="content">Content</label>
+        <textarea
+          name="content"
+          value={content}
+          onChange={({ target }) => setContent(target.value)}
+          id="1"
+          cols={30}
+          rows={10}
+        ></textarea>
+        <button type="submit">Create Blog</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
