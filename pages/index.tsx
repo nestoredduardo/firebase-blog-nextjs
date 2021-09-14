f499abfd6436749da6c4597220e0674d1499d533
Nestor Mamani <nmamanipantoja@gmail.com>
2021-09-13 23:40:03 -0500

    Home Login, Sign up, Logout

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,15 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 import CreatePost from "@components/CreatePost";
 import firebaseConfig from "@config/fire-conf";
 
 export default function Home() {
   const [blogs, setBlogs] = useState([]);
+  const [usermail, setUsermail] = useState("");
+  const [notify, setNotify] = useState("");
 
   useEffect(async () => {
     const firebaseApp = initializeApp(firebaseConfig);
@@ -23,13 +26,46 @@ export default function Home() {
       });
     });
     setBlogs(blogs);
+
+    const auth = getAuth(firebaseApp);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUsermail(user.email);
+      } else {
+        console.log("No user");
+      }
+    });
   }, []);
+
+  const handleLogout = async () => {
+    const firebaseApp = initializeApp(firebaseConfig);
+    await getAuth(firebaseApp).signOut();
+    setUsermail("");
+    setNotify("Logged out");
+    setTimeout(() => {
+      setNotify("");
+    }, 2000);
+  };
+
   return (
     <div>
       <Head>
         <title>Blog App</title>
       </Head>
       <h1>Blog</h1>
+      {notify}
+      {!usermail ? (
+        <div>
+          <Link href="/users/register">
+            <a>Sign up</a>
+          </Link>
+          <Link href="/users/login">
+            <a>Login</a>
+          </Link>
+        </div>
+      ) : (
+        <button onClick={handleLogout}>Logout</button>
+      )}
       <ul>
         {blogs.map((blog) => {
           return (
